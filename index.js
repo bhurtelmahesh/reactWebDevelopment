cbae64228578b9f8781f6a585836175e9773f3ed
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-24 22:19:08 +0900

    Practice rendering

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+//ReactDOM.render(<h1>Hello, World!</h1>, document.getElementById('root'));
+//ReactDOM.render(<p>Hi, My Name is Maheshwor!</p>, document.querySelector('#root'));
+MainComponent = () => {
+    return(
+        <div>
+            <h1>I'm Learning React!</h1>
+        </div>
+    )
+}
 
+ReactDOM.render(<MainComponent />, document.querySelector('#root'));
