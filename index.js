0676545d1d33a9e95ace404f2d24b8de7d99b839
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-25 00:46:32 +0900

    simple changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const page = (
             <li>Was first released in 2013</li>
             <li>Was originally created by Jordan Walke</li>
             <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Meta(formerly Facebook)</li>
+            <li>Is maintained by Meta (Facebook)</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
     </div>
