247ad360cab7a983f2d16637b64b5271df6d719c
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-25 18:33:15 +0900

    Learning

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const MyPage = () => {
     return(
     <div>
         <img src="react.png" alt="React Logo" width="60px" />
-        <h1> Fun Facts About React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Meta (Facebook)</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+        <h1> Reasons why I'm excited to learn React</h1>
+        <ol>
+            <li>It is the most used JavaScript Library</li>
+            <li>It is the most popular JavaScript Library</li>
+            <li>It is a very desirable skill</li>
+            <li>It is a very hirable skill</li>
+            <li>Powers thousands of enterprise apps, including mobile apps, so that the job opportunity is pretty high</li>
+        </ol>
     </div>
     );
 }
