149ac112537426009a662f8d975f62094101be20
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-25 18:53:04 +0900

    learning

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const MyPage = () => {
     return(
     <div>
-        <img src="react.png" alt="React Logo" width="60px" />
+        <header>
+            <nav>
+                <img src="react.png" alt="React Logo" width="60px" />
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>       
+            </nav>
+        </header>
         <h1> Reasons why I'm excited to learn React</h1>
         <ol>
             <li>It is the most used JavaScript Library</li>
             <li>It is the most popular JavaScript Library</li>
-            <li>It is a very desirable skill</li>
-            <li>It is a very hirable skill</li>
+            <li>It is a very desireable skill, I'm more likely to get a job</li>
+            <li>It is a very hireable skill</li>
             <li>Powers thousands of enterprise apps, including mobile apps, so that the job opportunity is pretty high</li>
         </ol>
+        <footer>
+            <p>&copy; 2022 Bhurtel Development. All  rights reserved.</p>
+        </footer>
     </div>
     );
 }
