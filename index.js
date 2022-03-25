43d98baec386390105c61b826048c7eda64a68eb
bhurtelmahesh <bhurtelace@gmail.com>
2022-03-25 19:01:37 +0900

    components

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const MyPage = () => {
     return(
     <div>
-        <header>
-            <nav>
-                <img src="react.png" alt="React Logo" width="60px" />
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>       
-            </nav>
-        </header>
+        <Header />
         <h1> Reasons why I'm excited to learn React</h1>
         <ol>
             <li>It is the most used JavaScript Library</li>
@@ -19,10 +10,30 @@ const MyPage = () => {
             <li>It is a very hireable skill</li>
             <li>Powers thousands of enterprise apps, including mobile apps, so that the job opportunity is pretty high</li>
         </ol>
-        <footer>
+        <Footer />
+    </div>
+    );
+}
+
+const Footer = () => {
+    return(
+        <footer >
             <p>&copy; 2022 Bhurtel Development. All  rights reserved.</p>
         </footer>
-    </div>
+    )
+}
+const Header = () => {
+    return(
+        <header>
+        <nav>
+            <img src="react.png" alt="React Logo" width="60px" />
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>       
+        </nav>
+    </header>
     );
 }
 
